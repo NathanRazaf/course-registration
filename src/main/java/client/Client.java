5e1a002da21f2e61158dabefb5201fd290fc17c6
NathanRazaf <nathan.razafindrakoto@umontreal.ca>
2023-04-04 11:04:27 -0400

    create sendRegistration function and complete alpha version of full Client

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,6 +1,7 @@
 package client;
 
 import server.models.Course;
+import server.models.RegistrationForm;
 
 import java.io.*;
 import java.net.ConnectException;
@@ -16,7 +17,6 @@ public class Client {
         try {
             System.out.println("*** Bienvenue au portail d'inscription de cours de l'UDEM ***");
             askCourses();
-
 
 
         } catch (ConnectException x) {
@@ -40,42 +40,106 @@ public class Client {
             case "1" -> {
                 fetchCourses("Automne");
                 currentSession = "Automne";
-            } case "2" -> {
+            }
+            case "2" -> {
                 fetchCourses("Hiver");
                 currentSession = "Hiver";
-            } case "3" -> {
+            }
+            case "3" -> {
                 fetchCourses("Ete");
                 currentSession = "Ete";
-            } default -> {
+            }
+            default -> {
+                System.out.println("Entrez un choix valide svp!");
+                askCourses();
+            }
+        }
+
+        System.out.println("> Choix:");
+        System.out.println("1. Consulter les cours offerts pour une autre session");
+        System.out.println("2. Inscription à un cours");
+        System.out.println("> Choix:");
+        Scanner sc1 = new Scanner(System.in);
+        String line1 = sc1.nextLine();
+        switch (line1) {
+            case "1" -> askCourses();
+            case "2" -> sendRegistration();
+            default -> {
                 System.out.println("Entrez un choix valide svp!");
                 askCourses();
             }
         }
     }
 
-    public static void fetchCourses(String session) {
-        try {
-            Socket cS = new Socket("127.0.0.1", 1337);
+        public static void fetchCourses (String session){
+            try {
+                Socket cS = new Socket("127.0.0.1", 1337);
 
-            ObjectOutputStream oos = new ObjectOutputStream(cS.getOutputStream());
-            oos.writeObject("CHARGER " + session);
+                ObjectOutputStream oos = new ObjectOutputStream(cS.getOutputStream());
+                oos.writeObject("CHARGER " + session);
 
-            ObjectInputStream ois = new ObjectInputStream(cS.getInputStream());
-            ArrayList<Course> courseList = (ArrayList<Course>) ois.readObject();
+                ObjectInputStream ois = new ObjectInputStream(cS.getInputStream());
+                ArrayList<Course> courseList = (ArrayList<Course>) ois.readObject();
 
-            if (session.equals("Ete")) {
-                System.out.println("Les cours offerts durant la session d'été sont : ");
-            } else {
-                System.out.println("Les cours offerts durant la session d'" + session.toLowerCase() + " sont : ");
+                if (session.equals("Ete")) {
+                    System.out.println("Les cours offerts durant la session d'été sont : ");
+                } else {
+                    System.out.println("Les cours offerts durant la session d'" + session.toLowerCase() + " sont : ");
+                }
+
+                for (int i = 1; i < courseList.size() + 1; i++) {
+                    System.out.println(i + ". " + courseList.get(i - 1).getCode() + "\t" + courseList.get(i - 1).getName());
+                }
+
+                cS.close();
+            } catch (IOException | ClassNotFoundException e) {
+                System.out.println(e);
             }
-
-            for (int i = 1; i < courseList.size() + 1; i++) {
-                System.out.println(i + ". " + courseList.get(i - 1).getCode() + "\t" + courseList.get(i - 1).getName());
-            }
-
-            cS.close();
-        } catch (IOException | ClassNotFoundException e) {
-            System.out.println(e);
         }
-    }
+
+        public static void sendRegistration () {
+            try {
+
+                Socket socket1 = new Socket("127.0.0.1", 1337);
+                System.out.println("Veuillez saisir votre prénom:");
+                String name = new Scanner(System.in).next();
+                System.out.println("Veuillez saisir votre nom:");
+                String lastName = new Scanner(System.in).next();
+                System.out.println("Veuillez saisir votre email:");
+                String email = new Scanner(System.in).next();
+                System.out.println("Veuillez saisir votre matricule:");
+                String matricule = new Scanner(System.in).next();
+                System.out.println("Veuillez saisir le code du cours:");
+                String codeCours = new Scanner(System.in).next();
+                Course course = null;
+                ObjectOutputStream output1 = new ObjectOutputStream(socket1.getOutputStream());
+                output1.writeObject("CHARGER " + currentSession);
+                ObjectInputStream input1 = new ObjectInputStream(socket1.getInputStream());
+                ArrayList<Course> courseList = (ArrayList<Course>) input1.readObject();
+                socket1.close();
+                for (Course coursePointed : courseList) {
+                    if (coursePointed.getCode().equals(codeCours)) {
+                        course = coursePointed;
+                        break;
+                    }
+                }
+                if (course == null) {
+                    throw new IllegalArgumentException("Ce cours n'existe pas dans la session sélectionnée");
+                }
+                RegistrationForm registrationForm = new RegistrationForm(name, lastName, email, matricule, course);
+                Socket socket2 = new Socket("127.0.0.1", 1337);
+
+                ObjectOutputStream output2 = new ObjectOutputStream(socket2.getOutputStream());
+                output2.writeObject("INSCRIRE");
+                output2.writeObject(registrationForm);
+
+
+                ObjectInputStream input2 = new ObjectInputStream(socket2.getInputStream());
+                System.out.println((String) input2.readObject());
+                socket2.close();
+
+            } catch (IOException | ClassNotFoundException e) {
+                System.out.println(e);
+            }
+        }
 }
