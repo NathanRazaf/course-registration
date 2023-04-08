f0bbffc3f35a4a38974d2b913322e650d4ceeb23
Cynthia Phan <cynthiaphan.code@gmail.com>
2023-04-07 21:26:49 -0400

    complete main method

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -14,17 +14,31 @@ public class Client {
     public final static String SUMMER_SEMESTER = "Ete";
     public final static String LOAD_COMMAND = "CHARGER";
     public final static String REGISTER_COMMAND = "INSCRIRE";
+    public static Scanner sc = new Scanner(System.in);
+    public static final String TEXT_RESET = "\u001B[0m";
+    public static final String TEXT_RED = "\u001B[31m";
+    public static final String TEXT_BLUE   = "\u001B[34m";
 
     public static void main(String[] args) {
-        System.out.println("*** Bienvenue au portail d'inscription de cours de l'UDEM ***");
-        String command;
+        try {
+            System.out.println(TEXT_BLUE + "*** Bienvenue au portail d'inscription de cours de l'UDEM ***" + TEXT_RESET);
 
-        /* TODO : run the program */
+            ArrayList<Course> courseList = loadCourses();
+            printCommandOptions();
+            String command = getCommand();
 
+            while (true) {
+                handleCommands(command, courseList);
+                printCommandOptions();
+                command = getCommand();
+            }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void printSemesterOptions() {
-        System.out.println("Veuillez choisir la session pour laquelle vous voulez consulter la liste de cours :");
+        System.out.println(TEXT_BLUE + "\nVeuillez choisir la session pour laquelle vous voulez consulter la liste de cours :" + TEXT_RESET);
         System.out.println("1. Automne");
         System.out.println("2. Hiver");
         System.out.println("3. Été");
@@ -34,36 +48,31 @@ public class Client {
     public static String getSemester() {
         String choice;
 
-        Scanner sc = new Scanner(System.in);
-
         while (true) {
             choice = sc.nextLine();
 
             if (choice.equals("1")) {
-                sc.close();
                 return FALL_SEMESTER;
             }
 
             if (choice.equals("2")) {
-                sc.close();
                 return WINTER_SEMESTER;
             }
 
             if (choice.equals("3")) {
-                sc.close();
                 return SUMMER_SEMESTER;
             }
 
-            System.out.println("Entrez un choix valide SVP!");
+            System.out.println("Entrez un choix valide SVP!\n");
             System.out.print("> Choix : ");
         }
     }
 
     public static void printCommandOptions() {
-        System.out.println("Veuillez choisir un choix parmi les suivants :");
+        System.out.println(TEXT_BLUE + "\nVeuillez choisir un choix parmi les suivants :" + TEXT_RESET);
         System.out.println("1. Consulter les cours offerts pour une autre session");
         System.out.println("2. Inscription");
-        System.out.print("> Choix :");
+        System.out.print("> Choix : ");
         /* TODO : print command options after success of registration to let user register to another course */
         /* TODO : add quit option */
     }
@@ -71,22 +80,18 @@ public class Client {
     public static String getCommand() {
         String choice;
 
-        Scanner sc = new Scanner(System.in);
-
         while (true) {
             choice = sc.nextLine();
 
             if (choice.equals("1")) {
-                sc.close();
                 return LOAD_COMMAND;
             }
 
             if (choice.equals("2")) {
-                sc.close();
                 return REGISTER_COMMAND;
             }
 
-            System.out.println("Entrez un choix valide SVP!");
+            System.out.println("Entrez un choix valide SVP!\n");
             System.out.print("> Choix : ");
         }
     }
@@ -113,11 +118,11 @@ public class Client {
 
     public static void printCourses(String semester, ArrayList<Course> courseList) {
         if (semester.equals(SUMMER_SEMESTER)) {
-            System.out.println("Les cours offerts durant la session d'été sont :");
+            System.out.println("\nLes cours offerts durant la session d'été sont :");
         }
 
         if (semester.equals(FALL_SEMESTER) || semester.equals(WINTER_SEMESTER)) {
-            System.out.println("Les cours offerts durant la session d'" + semester.toLowerCase() + " sont :");
+            System.out.println("\nLes cours offerts durant la session d'" + semester.toLowerCase() + " sont :");
         }
 
         for (int i = 1; i < courseList.size() + 1; i++) {
@@ -126,15 +131,20 @@ public class Client {
     }
 
     public static RegistrationForm fillForm(ArrayList<Course> courseList) {
-        Scanner sc = new Scanner(System.in);
-        String name, lastName, email, studentID, courseID;
+        String firstName, lastName, email, studentID, courseID;
         Course course = null;
 
         while (true) {
-            System.out.print("Veuillez saisir votre prénom : ");
-            name = sc.nextLine();
+            System.out.print("\nVeuillez saisir votre prénom : ");
+            firstName = sc.nextLine();
 
-            if (!(Character.isUpperCase(name.charAt(0)))) {
+            if (firstName.isBlank()) {
+                System.out.println(TEXT_RED + "Un prénom est requis!" + TEXT_RESET);
+            } else if (firstName.matches(".*[0-9!@#$%&*()_+=|<>?{}/\\\\~].*")) {
+                System.out.println("Le prénom n'est pas conforme!");
+            } else if (firstName.startsWith(" ") || firstName.endsWith(" ")) {
+                System.out.println("Un prénom ne doit pas contenir d'espaces au début et à la fin!");
+            } else if (!Character.isUpperCase(firstName.charAt(0))) {
                 System.out.println("Un prénom doit commencer par une majuscule!");
             } else {
                 break;
@@ -142,10 +152,16 @@ public class Client {
         }
 
         while (true) {
-            System.out.println("Veuillez saisir votre nom : ");
+            System.out.print("\nVeuillez saisir votre nom : ");
             lastName = sc.nextLine();
 
-            if (!(Character.isUpperCase(lastName.charAt(0)))) {
+            if (lastName.isBlank()) {
+                System.out.println(TEXT_RED + "Un nom est requis!" + TEXT_RESET);
+            } else if (lastName.matches(".*[0-9!@#$%&*()_+=|<>?{}/\\\\~].*")) {
+                System.out.println("Le nom n'est pas conforme!");
+            }else if (lastName.startsWith(" ") || lastName.endsWith(" ")) {
+                System.out.println("Un nom ne doit pas contenir d'espaces au début et à la fin!");
+            } else if (!Character.isUpperCase(lastName.charAt(0))) {
                 System.out.println("Un nom doit commencer par une majuscule!");
             } else {
                 break;
@@ -153,42 +169,46 @@ public class Client {
         }
 
         while (true) {
-            System.out.println("Veuillez saisir votre e-mail : ");
+            System.out.print("\nVeuillez saisir votre e-mail : ");
             email = sc.nextLine();
 
-            if (!email.contains("@") || email.indexOf("@") == 0 || email.lastIndexOf(".") < email.indexOf("@") ||
-                    email.lastIndexOf(".") == email.length() - 1) {
-                System.out.println("Le-mail entré est non conforme!");
+            if (email.isBlank()) {
+                System.out.println(TEXT_RED + "Un e-mail est requis!" + TEXT_RESET);
+            } else if (email.contains(" ") || !email.contains("@") || email.indexOf("@") == 0 ||
+                    email.lastIndexOf(".") < email.indexOf("@") || email.lastIndexOf(".") == email.length() - 1 
+                    || email.matches(".*[!#$%&*()+=|<>?{}/\\\\~].*")) {
+                System.out.println("L'e-mail entré est non conforme!");
             } else {
                 break;
             }
         }
 
         while (true) {
-            System.out.println("Veuillez saisir votre matricule : ");
+            System.out.print("\nVeuillez saisir votre matricule : ");
             studentID = sc.nextLine();
 
-            for  (int i = 0; i < studentID.length() - 1; i++) {
-                if (!Character.isDigit(studentID.charAt(i))) {
-                    System.out.println("Le matricule doit contenir seulement des chiffres!");
-                    break;
-                }
-            }
-
-            if (studentID.length() != 8) {
-                System.out.println("Le matricule doit contenir exactement 8 chiffres!");
+            if (studentID.isBlank()) {
+                System.out.println(TEXT_RED + "Un matricule est requis!" + TEXT_RESET);
+            } else if (studentID.matches(".*[a-zA-z!@#$%&*()_+=|<>?{}/\\\\~-].*")) {
+                System.out.println("Un matricule doit contenir des chiffres seulement!");
+            } else if (studentID.contains(" ")) {
+                System.out.println("Un matricule ne doit pas contenir des espaces!");
+            } else if (!studentID.matches(".*[a-zA-z!@#$%&*()_+=|<>?{}/\\\\~-].*") && studentID.length() != 8) {
+                System.out.println("Un matricule doit contenir exactement 8 chiffres!");
             } else {
                 break;
             }
         }
 
         while (true) {
-            System.out.println("Veuillez saisir le code du code : ");
+            System.out.print("\nVeuillez saisir le code du code : ");
             courseID = sc.nextLine();
 
             String courseListString = courseList.toString();
 
-            if (!courseListString.contains(courseID)) {
+            if (courseID.isBlank()) {
+                System.out.println(TEXT_RED + "Un code de cours est requis!" + TEXT_RESET);
+            } else if (!courseListString.contains(courseID)) {
                 System.out.println("Ce cours n'existe pas dans la session sélectionnée!");
             } else {
                 for (Course coursePointed : courseList) {
@@ -201,9 +221,7 @@ public class Client {
             }
         }
 
-        sc.close();
-
-        return new RegistrationForm(name, lastName, email, studentID, course);
+        return new RegistrationForm(firstName, lastName, email, studentID, course);
     }
 
     public static void sendRegistration(RegistrationForm form) {
@@ -223,11 +241,12 @@ public class Client {
         }
     }
 
-    public static void loadCourses() {
+    public static ArrayList<Course> loadCourses() {
         printSemesterOptions();
         String semester = getSemester();
         ArrayList<Course> courseList = fetchCourses(semester);
         printCourses(semester, courseList);
+        return courseList;
     }
 
     public static void register(ArrayList<Course> courseList) {
